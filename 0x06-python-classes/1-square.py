d56348147be73534c1d582e0eed9d250b92b98da
Keely001 <thindiuke@gmail.com>
2023-08-29 21:04:27 +0300

    task 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,6 +2,11 @@
 """a class Square that defines a square by: (based on 0-square.py)"""
 
 class Square:
+    """reps the square"""
     def __init__(self, size):
+        """this is to initialize the square.
+        
+        Args:
+            size (int): The size"""
         self.__size = size
 
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""The Square"""
+
+
+class Square:
+    """reps the square"""
+
+    def __init__(self, size=0):
+        """Initialize the square.
+
+        Args:
+            size (int): size of square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
