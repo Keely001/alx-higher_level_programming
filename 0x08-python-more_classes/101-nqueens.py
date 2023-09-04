8183f9c3dd716c6f31b555248e6a72d88313ee85
Keely001 <thindiuke@gmail.com>
2023-09-04 22:37:19 +0300

    extra

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,31 @@
+101-nqueens.py
+#!/usr/bin/python3
+"""
+The N queens puzzle is the challenge of placing N
+non-attacking queens on an N×N chessboard. 
+Write a program that solves the N queens problem.
+"""
+import sys
+
+
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = init_board(int(sys.argv[1]))
+    
