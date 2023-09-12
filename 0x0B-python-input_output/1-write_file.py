117bbb34c193b14891c4c944a4b86aefee803e98
Keely001 <thindiuke@gmail.com>
2023-09-12 13:32:17 +0300

    remove semis

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,7 +9,7 @@ def write_file(filename="", text=""):
         text:text
     returns:
         the number of characters written"""
-    chars_num = 0;
+    chars_num = 0
     with open(filename, 'w', encoding='utf-8') as file:
         chars_num = file.write(text)
     return(chars_num)
