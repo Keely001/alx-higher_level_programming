1986d30592218380bec2aaae293729a1bcd6cc3a
Keely001 <126445439+Keely001@users.noreply.github.com>
2023-10-18 14:00:06 +0300

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,6 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
+SET PASSWORD FOR 'user_0d_2'@'%' = 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
 FLUSH PRIVILEGES;
