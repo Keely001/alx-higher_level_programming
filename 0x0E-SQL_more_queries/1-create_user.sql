2014da791dc93fe8abd4801a19b594d41ded5494
Keely001 <126445439+Keely001@users.noreply.github.com>
2023-10-18 13:49:43 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,4 @@
 -- Query to create the user 'user_0d_1' in MySQL server
--- Create user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to the user
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
-
--- Reload privileges to apply the changes
 FLUSH PRIVILEGES;
