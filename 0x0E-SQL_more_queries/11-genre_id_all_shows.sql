9ccffa5fd6ae36bc866e5c9125f36ec0688cced9
Keely001 <126445439+Keely001@users.noreply.github.com>
2023-10-18 22:24:45 +0300

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 --Lists all cities contained in the database
-SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join cities and states
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') as genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ORDER BY tv_shows.title, genre_id;
