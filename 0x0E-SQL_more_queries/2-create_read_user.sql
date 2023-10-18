16a46eba7832fabbb8258005928b9923d155402b
Keely001 <126445439+Keely001@users.noreply.github.com>
2023-10-18 13:51:26 +0300

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,6 @@
 -- Creates the user 'user_0d_1' and the database 'hbtn_0d_2' in MySQL server
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
-GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost'
-IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
 FLUSH PRIVILEGES;
